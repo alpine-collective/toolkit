481e57c73d8a2a60718f5fa033c41afc84fd24d7
Ryan Chandler <ryangjchandler@gmail.com>
2021-06-10 21:46:00 +0100

    feature: unsafe modifier

--- packages/x-html/src/index.js
+++ packages/x-html/src/index.js
@@ -5,7 +5,35 @@ export default function (Alpine) {
         effect(() => {
             getHtml(html => {
                 el.innerHTML = html
+
+                if (modifiers.includes('unsafe')) {
+                    nodeScriptReplace(el)
+                }
             })
         })
     })
+}
+
+const nodeScriptClone = function (node) {
+    const script = document.createElement('script')
+    script.text = node.innerHTML
+
+    for (let i = 0; i < node.attributes.length; i++) {
+        const attr = node.attributes[i]
+        script.setAttribute(attr.name, attr.value)
+    }
+
+    return script
+}
+
+const nodeScriptReplace = function (node) {
+    if (node.tagName && node.tagName.toLowerCase() === 'script') {
+        node.parentNode.replaceChild(nodeScriptClone(node), node)
+    } else {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            nodeScriptReplace(node.childNodes[i])
+        }
+    }
+
+    return node
 }
