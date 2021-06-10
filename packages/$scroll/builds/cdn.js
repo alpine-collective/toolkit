833d9efd14932911b759ffbe9eab14ad9722e084
Simone Todaro <simone.todaro@gear4music.com>
2021-06-10 22:35:39 +0100

    feature: $scroll helper

---
+++ packages/$scroll/builds/cdn.js
@@ -0,0 +1,5 @@
+import Scroll from '../src/index'
+
+document.addEventListener('alpine:initializing', () => {
+    Scroll(window.Alpine)
+})
---
+++ packages/$scroll/builds/module.js
@@ -0,0 +1,3 @@
+import Scroll from '../src/index'
+
+export default Scroll
